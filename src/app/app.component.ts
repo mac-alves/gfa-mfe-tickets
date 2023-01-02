1ff5732e89e6e217fa400663eacac96df601320d
Mauricio <maurcioalves@ciandt.com>
2023-01-02 10:40:06 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gfa-mfe-tickets-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gfa-mfe-tickets';
+}
