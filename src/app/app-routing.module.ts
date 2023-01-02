d857f5152d19fabcc6f2444b937c0d094a78e530
Mauricio <maurcioalves@ciandt.com>
2023-01-02 11:35:48 -0300

    add single-spa-angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { APP_BASE_HREF } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: EmptyRouteComponent }
+];
 
 @NgModule({
+  exports: [RouterModule],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
 })
 export class AppRoutingModule { }
---
+++ src/app/empty-route/empty-route.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gfa-mfe-tickets-empty-route',
+  template: '',
+})
+export class EmptyRouteComponent {}
